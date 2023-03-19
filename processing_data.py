89aac72528dbb884a46d753762f96ac8abce4643
Vatika Agrawal <100186186+vatika17@users.noreply.github.com>
2023-03-20 03:32:36 +0530

    Update processing_data.py

--- processing_data.py
+++ processing_data.py
@@ -11,7 +11,7 @@ import mediapipe as mp
 import cv2
 import matplotlib.pyplot as plt
 
-
+# Load the Land handmark model
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -19,7 +19,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 DATA_DIR = './data'
-
+# read image from directory
 data = []
 labels = []
 for dir_ in os.listdir(DATA_DIR):
@@ -30,9 +30,11 @@ for dir_ in os.listdir(DATA_DIR):
         y_ = []
 
         img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+#converting image to RGB
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
+# Process image and detect land handmark 
         results = hands.process(img_rgb)
+# Storing the hand landmarks(x and y) of the images in an array. This array will represent the image. And the labels will be the name of the directory the image is present in:
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 for i in range(len(hand_landmarks.landmark)):
@@ -50,7 +52,7 @@ for dir_ in os.listdir(DATA_DIR):
 
             data.append(data_aux)
             labels.append(dir_)
-
+# Saving the data in filename data.pickle and creating a dictionary with keys ‘data’ and ‘labels’:
 f = open('data.pickle', 'wb')
 pickle.dump({'data': data, 'labels': labels}, f)
 f.close()
