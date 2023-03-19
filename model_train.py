f36da2e8ea9bfbf729b9176f6f5516f8320b9ba9
Vatika Agrawal <100186186+vatika17@users.noreply.github.com>
2023-03-20 03:35:05 +0530

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -3,7 +3,7 @@
 
 # In[3]:
 
-
+# function for computing accuracy
 def compute_accuracy(Y_true, Y_pred):  
     correctly_predicted = 0  
     # iterating over every label and checking it with the true sample  
@@ -28,20 +28,21 @@ from sklearnex.model_selection import train_test_split
 from sklearnex import metrics
 import numpy as np
 
-
+# Loading the data and converting the ‘data’ and ‘label’ list into numpy arrays:
 data_dict = pickle.load(open('./data.pickle', 'rb'))
 
 data = np.asarray(data_dict['data'])
 labels = np.asarray(data_dict['labels'])
-
+# Splitting the datasets into training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
+# Creating the model using random forest classifier and training the model with the training dataset
 model = RandomForestClassifier()
 
 model.fit(x_train, y_train)
-
+# Making predictions on new data points
 y_predict = model.predict(x_test)
-
+# Computing the accuracy of the model
 score = compute_accuracy(y_test,y_predict)
 
 print('{}% of samples were classified correctly !'.format(score * 100))
