48604102df1a8bb2e87044f1a15b56c18b1a8343
Vatika Agrawal <100186186+vatika17@users.noreply.github.com>
2023-03-29 20:41:18 +0530

    Update collecting_data.py

--- collecting_data.py
+++ collecting_data.py
@@ -27,7 +27,7 @@ for j in range(number_of_classes):
     done = False
     while True:
         ret, frame = cap.read()
-        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+        cv2.putText(frame, 'Press "Q" to start', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
                     cv2.LINE_AA)
         cv2.imshow('frame', frame)
         if cv2.waitKey(25) == ord('q'):
